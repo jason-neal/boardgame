1a238a99ea0d12bb01fc75df8eaee1de5839ca9e
Jason Neal <jason.neal@astro.up.pt>
2018-05-16 22:20:08 +0100

    Limit minimum dice sides=2

--- dice.py
+++ dice.py
@@ -3,17 +3,19 @@ import random
 class Dice(object):
 
     def __init__(self, sides=6):  
-        if isinstance(sides, int):
-            self.sides = sides
-        else: 
+        if not isinstance(sides, int):
             raise TypeError("Number of sides must be a int")
+        elif sides < 2:
+            raise ValueError("Number of sides must be >=2")
+        self.sides = sides
 
     def roll(self):
-        """"""
+        """Roll the dice and return the value."""
         return random.randint(1, self.sides)
 
     def rolln(self, n=1):
         """Roll n dice.
+
         Returned as a list.
         """
         return [random.randint(1, self.sides) for _ in range(n)]
--- test_dice.py
+++ test_dice.py
@@ -1,10 +1,10 @@
 from dice import Dice
 import pytest
 
-@pytest.fixture
-def dice():
+@pytest.fixture(params=[2, 4, 6, 24])
+def dice(request):
    """Return a dice object"""
-   return Dice()
+   return Dice(sides=request.param)
 
 def test_Dice_type():
     d = Dice()
@@ -17,10 +17,25 @@ def test_dice_sides(s):
     assert d.sides == s
 
 
+@pytest.mark.parametrize("s", [0.5, 2.0, "6", "7.0", None])
+def test_dice_with_noninteger_sides(s):
+    """Should raise TypeError when side is non-integer."""
+    with pytest.raises(TypeError):
+        Dice(sides=s)
+
+
+@pytest.mark.parametrize("s", [True, False, -1, -5, 1, 0])
+def test_dice_with_small_sides(s):
+    """Should raise ValueError when side is less than 2."""
+    with pytest.raises(ValueError):
+        Dice(sides=s)
+
+
 def test_dice_roll(dice):
     result = dice.roll()
     assert result <= dice.sides
     assert result >= 1
+
 
 @pytest.mark.parametrize("n", [1,2,3,4,5,6,500])
 def test_dice_rolln(dice, n):
